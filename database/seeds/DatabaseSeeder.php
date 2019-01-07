002ac850b1ba4536ba0bbced2855602b0d5741f0
AvBorikDeveloper <avboris28@gmail.com>
2019-01-08 00:41:56 +1300

    init commit, installed modules, created db

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        // Creates 10 posts in database
+        factory(Post::class, 10)->create();
+    }
+}
